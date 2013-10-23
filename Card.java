d92972921f157f0381a777cd8dca71d14cefb1b6
Kayvon Farzaneh <kayvon@cs.utexas.edu>
2013-10-23 15:07:08 -0500

    created the deck in Deck class, nothing in Hand currently, added a test in Game to make sure its been shuffled (Debuging)

--- Card.java
+++ Card.java
@@ -1,75 +1,38 @@
 import java.util.*;
 
+// public enum Value {
+// 	"Ace","2","3","4","5","6","7","8","9","10","King","Queen","King" 
+// }
+// public enum Type{
+// 	"Heart", "Diamond", "Club", "Spade"
+// }
+// public enum Color{
+// 	"Red", "Black"
+// }
+
 public class Card
 {
-	private static String value;
-	private static String type;
-	private static String color;
 
-	private Card()
+	public String value;
+	public String type;
+	public String color;
+
+	public Card()
 	{
 		value = null;
 		type = null;
 		color = null;
 	}
-	public Card(int val, int t)
+	public Card(String val, String t, String col)
 	{
+		value = val;
+		type = t;
+		color = col;
 		//System.out.println(val + " " +t);
-		switch(val)
-		{
-		 	case 0:value = "Ace";
-		 	break;
-		 	case 1:value = "2";
-		 	break;
-		 	case 2:value = "3";
-		 	break;
-		 	case 3:value = "4";
-		 	break;
-		 	case 4:value = "5";
-		 	break;
-		 	case 5:value = "6";
-		 	break;
-		 	case 6:value = "7";
-		 	break;
-		 	case 7:value = "8";
-		 	break;
-		 	case 8:value = "9";
-		 	break;
-		 	case 9:value = "10";
-		 	break;
-		 	case 10:value = "Jack";
-		 	break;
-		 	case 11:value = "Queen";
-		 	break;
-		 	case 12:value = "King";
-		 	break;
-		}
-		switch(t)
-		{
-		 	case 0:type = "Heart";
-		 	break;
-		 	case 1:type = "Diamond";
-		 	break;
-		 	case 2:type = "Club";
-		 	break;
-		 	case 3:type = "Spade";
-		 	break;
-		}
-		switch(t)
-		{
-		 	case 0:color = "Red";
-		 	break;
-		 	case 1:color = "Red";
-		 	break;
-		 	case 2:color = "Red";
-		 	break;
-		 	case 3:color = "Red";
-		 	break;
-		}
 	}
 
-	public static String getValue() { return value;}
-	public static String getType() { return type;}
-	public static String getColor() { return color;}
+	public String getValue() { return value;}
+	public String getType() { return type;}
+	public String getColor() { return color;}
 
 }
--- Game.java
+++ Game.java
@@ -2,20 +2,32 @@ import java.util.*;
 
 public class Game
 {
-	public static Stack<Card> createDeck()
+
+	// public static Stack<Card> createDeck()
+	// {
+		// Stack<Card> deck1 = new Stack<Card>();
+	// 	for(int i = 0; i< 13;i++)
+	// 	{
+	// 		for(int j = 0; j<4;j++)
+	// 		{
+	// 			Card temp = new Card(i,j);
+	// 			//System.out.println(temp.getValue() + " " + temp.getType());
+	// 			deck1.push(temp);
+	// 		}
+	// 	}
+	// 	for(int i = 0; i<52;i++)
+	// 	{
+	// 		System.out.println(deck1.peek().getValue() + " " + deck1.pop().getType());
+	// 	}
+	// 	return deck1;
+	// }
+	public static void TestShuffled(Deck deck)
 	{
-		Stack<Card> deck = new Stack<Card>();
-		for(int i = 0; i< 13;i++)
+		while(!deck.empty())
 		{
-			for(int j = 0; j<4;j++)
-			{
-				Card temp = new Card(i,j);
-		//		System.out.println(temp.getValue() + " " + temp.getType());
-				deck.push(temp);
-			}
+			Card temp = deck.pop();
+			System.out.println(" Card is a : " + temp.getValue() + " of " + temp.getType());
 		}
-		Collections.shuffle(deck);
-		return deck;
 	}
 
 	public static Card Hit(Stack<Card> deck)
@@ -26,6 +38,9 @@ public class Game
 
 	public static void main(String[] args)
 	{
-		Stack<Card> deck = createDeck();
+		Deck deck = new Deck();
+
+		//uncomment to test that the deck has been created and shuffled
+		TestShuffled(deck);
 	}
 }
